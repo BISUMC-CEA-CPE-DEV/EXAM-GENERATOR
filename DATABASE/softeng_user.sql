66d4c7aa5d3e2998f3fe79ec8c908822be13a449
Engr. Max Angelo Perin <perinmaks@outlook.ph>
2020-03-10 10:46:40 +0800

    reanamed database

--- DATABASE/softeng_user.sql
+++ DATABASE/softeng_user.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Ultimate v9.62 
-MySQL - 5.6.37-log : Database - user
+MySQL - 5.5.54 : Database - softeng_user
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.6.37-log : Database - user
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`user` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`softeng_user` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
-USE `user`;
+USE `softeng_user`;
 
 /*Table structure for table `account` */
 
